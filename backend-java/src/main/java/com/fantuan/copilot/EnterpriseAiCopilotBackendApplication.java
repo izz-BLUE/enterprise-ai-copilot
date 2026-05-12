4493820007975d82499f5ebb7b1d1319b9217600
fantuan <2277281992@qq.com>
2026-05-13 02:09:59 +0800

    init spring boot backend

---
+++ backend-java/src/main/java/com/fantuan/copilot/EnterpriseAiCopilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fantuan.copilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseAiCopilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseAiCopilotBackendApplication.class, args);
+	}
+
+}
---
+++ backend-java/src/test/java/com/fantuan/copilot/EnterpriseAiCopilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantuan.copilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseAiCopilotBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
